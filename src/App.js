e06a2548fc3309e77f4298293600843561cce626
jeymes <87048226+jeymes@users.noreply.github.com>
2022-03-06 16:09:31 -0300

    full calculator

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Calculadora from './main';
+
+function App() {
+  return (
+    <div>
+      <h1>Calculadora</h1>
+      <Calculadora/>
+    </div>
+  );
+}
+
+export default App;
